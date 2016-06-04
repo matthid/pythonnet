a6fd27abf8906accdb6307f1d215e04b3017d3a7
Matthias Dittrich <matthi.d@gmail.com>
2016-06-04 14:59:45 +0200

    add test for using null like 'None' for default arguments. See 35cd5fa

---
+++ src/tests/test_suite/test_callback.py
@@ -0,0 +1,28 @@
+import unittest, sys
+import clr
+
+this_module = sys.modules[__name__]
+clr.AddReference("Python.Test")
+import Python.Test as Test
+from Python.Test import CallbackTest
+test_instance = CallbackTest()
+
+def simpleDefaultArg(arg = 'test'):
+    return arg
+
+class CallbackTests(unittest.TestCase):
+    """Test that callbacks from C# into python work."""
+
+    def testDefaultForNull(self):
+        """Test that C# can use null for an optional python argument"""
+        retVal = test_instance.Call_simpleDefaultArg_WithNull(__name__)
+        self.assertEquals(retVal, 'test')
+
+    def testDefaultForNone(self):
+        """Test that C# can use no argument for an optional python argument"""
+        retVal = test_instance.Call_simpleDefaultArg_WithEmptyArgs(__name__)
+        self.assertEquals(retVal, 'test')
+
+def test_suite():
+    return unittest.makeSuite(CallbackTests)
+
